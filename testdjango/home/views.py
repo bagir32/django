36e01944ba92cad0a2057662189a3823b1c5f3c4
Tagwa Abdelrahman <tagwaesam5@gmail.com>
2020-09-20 13:08:44 -0400

    update other.html

--- testdjango/home/views.py
+++ testdjango/home/views.py
@@ -11,7 +11,7 @@ def other(request):
     context = {
     'k1': 'Welcome to the Second page',
     }
-    return render(request, 'others.html', {'k1':context})
+    return render(request, 'others.html', {'context':context})
 
 
 def about(request):
